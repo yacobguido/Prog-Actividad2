231d66698c11823842f6ba04ffae127a9a97c775
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-03-19 20:36:54 -0300

    Update equipo.ts

--- equipo.ts
+++ equipo.ts
@@ -2,33 +2,17 @@ import { clearScreenDown } from "readline";
 
 const fs = require("fs");
 
-export const depositoDeEquipos: any [] = [] 
+ 
 export class Equipo{
     public id: string = " ";
     private descripcion: string= " ";
     private fechaFabricacion: Date = new Date();
     private fechaInstalacion: Date = new Date()
 
-    constructor(){
-            
+    constructor(id: string, descripcion: string, fechaFabricacion: string, fechaInstalacion: string){
+        this.id = id;
+        this.descripcion = descripcion;
+        this.fechaFabricacion = new Date (fechaFabricacion) ;
+        this.fechaInstalacion = new Date(fechaInstalacion);
     }
- 
-    public agregarEquipo(tipoEquipo: Equipo, id: string, descripcion: string, fechaFabricacion:string, fechaInstalacion: string){
-            this.id = id;
-            this.descripcion = descripcion;
-            this.fechaFabricacion = new Date (fechaFabricacion) ;
-            this.fechaInstalacion = new Date(fechaInstalacion);
-            depositoDeEquipos.push(tipoEquipo)
-            const datoALlevar= JSON.stringify(depositoDeEquipos);
-            const llevar = fs.writeFileSync('./deposito.json', datoALlevar,
-                 {
-                     encoding: "utf8",
-                     mode: 0o666
-                 }
-            );
-            
-        }
   }
-
-
-
