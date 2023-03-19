fa3403e100a7f4e174bdde0648c29864a1ad4e89
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-03-19 20:38:42 -0300

    Update motor.ts

--- motor.ts
+++ motor.ts
@@ -1,17 +1,45 @@
 import { Equipo } from "./equipo";
-enum eficiencia {
+export enum eficiencia {
     A = "Re eficiente", B = "bastante eficiente", C = "eficiente y nada mas", D = "poco eficiente", E = "Nada eficiente" 
+}
+export enum combustible{
+    N = "nafta", G = "gasoil", GNC = "gas natural comprimido"
 }
 
 export class Motor extends Equipo{
-    private potenciaEnHp: number;
-    private eficiencia: eficiencia;
-    private fabricante: string;
+    protected potenciaEnHp: number;
+    protected eficienciA: eficiencia;
+    protected fabricante: string;
 
-    constructor(potenciaEnHp: number, eficiencia: eficiencia, fabricante: string){
-        super()
+    constructor(potenciaEnHp: number, eficiencia: eficiencia, fabricante: string, id: string, descripcion: string, fechaFabricacion:string, fechaInstalacion: string){
+        super( id, descripcion, fechaFabricacion, fechaInstalacion)
         this.potenciaEnHp = potenciaEnHp;
-        this.eficiencia = eficiencia;
+        this.eficienciA = eficiencia;
         this.fabricante = fabricante;
+    }
+    
+}
+
+
+export class MotorC extends Motor{
+    cilindros: number
+    tipoCombustible: combustible
+
+    constructor(potenciaEnHp: number, eficienciA: eficiencia, fabricante: string, cilindros: number, tipoCombustible: combustible, id: string, descripcion: string, fechaFabricacion:string, fechaInstalacion: string){
+        super(potenciaEnHp, eficienciA, fabricante, id, descripcion, fechaFabricacion, fechaInstalacion)
+        this.cilindros = cilindros;
+        this.tipoCombustible = tipoCombustible;
+    }
+}
+
+export class MotorE extends Motor{
+    voltaje: string;
+    consumoKWH: number;
+    esBajoConsumo: boolean;
+    constructor(potenciaEnHp: number, eficienciA: eficiencia, fabricante: string, voltaje:string, consumoKWH: number, esBajoConsumo: boolean, id: string, descripcion: string, fechaFabricacion:string, fechaInstalacion: string){
+        super(potenciaEnHp, eficienciA, fabricante, id, descripcion, fechaFabricacion, fechaInstalacion)
+        this.voltaje = voltaje;
+        this.consumoKWH = consumoKWH;
+        this.esBajoConsumo = esBajoConsumo;
     }
 }
